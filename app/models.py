8e5b80f04ae215904a08ee391282030ac274ae31
Judhunja <judxhunja@gmail.com>
2024-08-19 23:15:44 +0300

    remove username from user table

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(db.Model):
     """db table model for class user"""
     __tablename__ = "user"
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
+    # username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 """ This module contains signup and login routes """
 from flask import (
-    Flask, render_template, flash, redirect, url_for, session, request
+    render_template, flash, redirect, url_for, session, request
 )
 from .models import db
 from .forms import LoginForm, SignupForm
@@ -40,9 +40,9 @@ def register_routes(app):
         form = LoginForm()
         if form.validate_on_submit():
             # get first user in db
-            user = User.query.filter_by(username=form.username.data).first()
+            user = User.query.filter_by(user_email=form.email.data).first()
             if user is not None and user.validate_password(form.password.data):
-                flash(f"{form.username.data} logged in successfully!")
+                flash(f"{form.email.data} logged in successfully!")
                 return redirect(url_for("home"))
             elif (
                 user is not None and
@@ -59,11 +59,11 @@ def register_routes(app):
         form = SignupForm()
         if form.validate_on_submit():
             # check for already existing username
-            username_exists = User.query.filter_by(
-                username=form.username.data).first()
-            if username_exists is not None:
+            """email_exists = User.query.filter_by(
+                email=form.email.data).first()
+            if email_exists is not None:
                 flash("Username already taken")
-                return render_template('signup.html', form=form)
+                return render_template('signup.html', form=form)"""
 
             # check for already existing email
             email_exists = User.query.filter_by(email=form.email.data).first()
@@ -72,7 +72,7 @@ def register_routes(app):
                 return render_template('signup.html', form=form)
 
             # if email and username are new, create a new User
-            user = User(username=form.username.data, email=form.email.data)
+            user = User(email=form.email.data)
             user.set_password(form.password.data)
             db.session.add(user)
             db.session.commit()
